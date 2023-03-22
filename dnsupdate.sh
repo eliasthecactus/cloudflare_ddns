02fb5ce0c51209b35a6a15eb38c46df18e63893f
elias <14819853+eliasthecactus@users.noreply.github.com>
2023-03-22 19:09:15 +0100

    Update dnsupdate.sh

--- dnsupdate.sh
+++ dnsupdate.sh
@@ -22,12 +22,11 @@ ZONE_ID=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=$DOMAIN_N
 
 # Get the Record ID for your A record
 RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A&name=$RECORD_NAME" \
-  -H "Authorization: Bearer $CF_API_KEY" \
-  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0].id')
+    -H "Authorization: Bearer $CF_API_KEY" \
+    -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0].id')
 
 # Update the A record with the new IP address
 curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
-  -H "X-Auth-Email: $CF_EMAIL" \
-  -H "X-Auth-Key: $CF_API_KEY" \
-  -H "Content-Type: application/json" \
-  --data "{\"type\":\"A\",\"name\":\"$RECORD_NAME\",\"content\":\"$IP\",\"proxied\":$PROXY_STATUS}"
+    -H "Authorization: Bearer $CF_API_KEY" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"A\",\"name\":\"$RECORD_NAME\",\"content\":\"$IP\",\"proxied\":$PROXY_STATUS}"

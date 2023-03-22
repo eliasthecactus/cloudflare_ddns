e9a323cb59498f580ccb3ca5d334141f00d38d2b
elias <14819853+eliasthecactus@users.noreply.github.com>
2023-03-22 18:39:38 +0100

    Update dnsupdate.sh

--- dnsupdate.sh
+++ dnsupdate.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-# Set variables
-CF_EMAIL="your@email.com"
-CF_API_KEY="your-api-key"
-CF_ZONE_NAME="yourdomain.com"
-CF_RECORD_NAME="your-record-name"
-IP=$(curl -s https://checkip.amazonaws.com/)
+#===============CHANGES START===============#
+# Set your Cloudflare email and API key
+CF_EMAIL="your_email@example.com"
+CF_API_KEY="your_api_key_here"
 
-# Get zone ID
-ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${CF_ZONE_NAME}" \
-  -H "X-Auth-Email: ${CF_EMAIL}" \
-  -H "X-Auth-Key: ${CF_API_KEY}" \
-  -H "Content-Type: application/json" | jq -r '.result[0].id')
+# Set your domain name and the A record you want to update
+DOMAIN_NAME="example.com"
+RECORD_NAME="www"
 
-# Get record ID
-RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?type=A&name=${CF_RECORD_NAME}" \
-  -H "X-Auth-Email: ${CF_EMAIL}" \
-  -H "X-Auth-Key: ${CF_API_KEY}" \
-  -H "Content-Type: application/json" | jq -r '.result[0].id')
+# record proxied (true) or not (false)
+PROXY_STATUS="false"
+#===============CHANGES END===============#
 
-# Update record
-curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${RECORD_ID}" \
-  -H "X-Auth-Email: ${CF_EMAIL}" \
-  -H "X-Auth-Key: ${CF_API_KEY}" \
+# Get the current public IP address
+IP=$(curl -s http://checkip.amazonaws.com)
+
+# Get the Zone ID for your domain
+ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$DOMAIN_NAME" \
+  -H "X-Auth-Email: $CF_EMAIL" \
+  -H "X-Auth-Key: $CF_API_KEY" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0].id')
+
+# Get the Record ID for your A record
+RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A&name=$RECORD_NAME" \
+  -H "X-Auth-Email: $CF_EMAIL" \
+  -H "X-Auth-Key: $CF_API_KEY" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0].id')
+
+# Update the A record with the new IP address
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
+  -H "X-Auth-Email: $CF_EMAIL" \
+  -H "X-Auth-Key: $CF_API_KEY" \
   -H "Content-Type: application/json" \
-  --data "{\"type\":\"A\",\"name\":\"${CF_RECORD_NAME}\",\"content\":\"${IP}\",\"ttl\":1,\"proxied\":false}"
+  --data "{\"type\":\"A\",\"name\":\"$RECORD_NAME\",\"content\":\"$IP\",\"proxied\":$PROXY_STATUS}"
